387daafb30a3c3e7aa8d647a5c88cea5e42f0d20
kocsen <kocsenc@gmail.com>
2014-04-26 12:50:00 -0400

    Remove output. finished digital tipewritter.py

--- april/DigitalTypewritter/DigitalTypewritter.py
+++ april/DigitalTypewritter/DigitalTypewritter.py
@@ -3,34 +3,40 @@
 # April 22, 2014
 import sys
 import fileinput
+import webbrowser
 
-
+FNAME = 'output.txt'
+MAXL = 72
 def main():
-    f = open('output.txt', 'w')
-    master_out = ""
+	f = open(FNAME, 'w')
+	master_out = ""
 
-    start_end_sequence = False
+	start_end_sequence = False
+	print("Typewritter | Start writing")
+	while (True):
+		line = input()
 
-    print("Typewritter | Start writing")
-    while (True):
-        line = input()
-        print("you have inputted: " + line)
-        # Listener for ending typewriter
-        if line == '':
-            start_end_sequence = True
-            print("Started end sequence")
-        else:
-            start_end_sequence = False
-            master_out += (line + "\n")
+		if line == '.' and start_end_sequence:
+			break
 
-        if start_end_sequence and line == ".":
-            print("You are done here")
-            break
+		# Listener for ending typewriter
+		if line == '':
+			start_end_sequence = True
+		else:
+			start_end_sequence = False
+			if len(line) > MAXL:
+				print("\tERROR: Limit of characters per line is 72, TRUNCATING")
+				master_out += (line[:MAXL] + '\n')
+			else:
+				master_out += (line + '\n')
 
-    f.write(master_out)
-    f.close()
+	f.write(master_out)
+	f.close()
 
+	# Printing out file
+	print("\nTypewriter | Openning result file")
+	webbrowser.open(FNAME)
 
 if __name__ == "__main__":
-    main()
+	main()
 
