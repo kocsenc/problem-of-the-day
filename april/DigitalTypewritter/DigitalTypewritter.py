4aeebcf09adb7eb5512c007aa33889a48181e6dc
Kocsen <kxc4519@rit.edu>
2014-04-26 11:56:23 -0400

    code reformat

--- april/DigitalTypewritter/DigitalTypewritter.py
+++ april/DigitalTypewritter/DigitalTypewritter.py
@@ -2,32 +2,32 @@
 # Problem of the day - Digital Typewritter
 # April 22, 2014
 import sys
-import fileinput 
+import fileinput
+
 
 def main():
-	f = open('output.txt', 'w')
-	master_out = ""
+    f = open('output.txt', 'w')
+    master_out = ""
 
-	start_end_sequence = False
+    start_end_sequence = False
 
-	print("Typewritter | Start writing")
-	while(True):
-		line = input()
-		print("you have inputted: " + line)
-		# Listener for ending typewriter
-		if line == "":
-			start_end_sequence = True
-		else:
-			start_end_sequence = False
-			master_out += (line + "\n")
+    print("Typewritter | Start writing")
+    while (True):
+        line = input()
+        print("you have inputted: " + line)
+        # Listener for ending typewriter
+        if line == "":
+            start_end_sequence = True
+        else:
+            start_end_sequence = False
+            master_out += (line + "\n")
 
-		if start_end_sequence and line == ".":
-			break
-		
+        if start_end_sequence and line == ".":
+            break
 
-	f.write(master_out)
-	f.close()
+    f.write(master_out)
+    f.close()
 
 
 if __name__ == "__main__":
-	main()
+    main()
